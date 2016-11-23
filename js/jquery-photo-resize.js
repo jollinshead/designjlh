31505f0aafd607a7dfbba75d584ce4f1e75e1ea4
jollinshead <jhollinshead1993@gmail.com>
2016-11-23 21:49:40 +0000

    enlarge image. dynamic arrow position

--- js/jquery-photo-resize.js
+++ js/jquery-photo-resize.js
@@ -17,7 +17,7 @@
 
         var element	= $(this),
             defaults = {
-                bottomSpacing: 200
+                bottomSpacing: 150
             };
 
 
@@ -36,20 +36,30 @@
                 browserHeight = $(window).height(),
                 browserWidth = $(window).width();
 
-            var smallestMeasurement = browserHeight < browserWidth ? browserHeight : browserWidth;
-            var newHeight = smallestMeasurement - o.bottomSpacing < 200 ? 200 : smallestMeasurement - o.bottomSpacing;
-            var newWidth = newHeight;
+            var smallestMeasurement = (browserHeight < browserWidth ? browserHeight : browserWidth) - 60;
+            //var newHeight = smallestMeasurement - o.bottomSpacing < 200 ? 200 : smallestMeasurement - o.bottomSpacing;
+            //var newWidth = newHeight;
 
-            $(element).attr('height', newHeight );
-            $(element).attr('width', newWidth );
-            $(element).attr('max-height', newHeight );
-            $(element).attr('max-width', newWidth );
+            $(element).attr('height', smallestMeasurement );
+            $(element).attr('width', smallestMeasurement );
+            $(element).attr('max-height', smallestMeasurement );
+            $(element).attr('max-width', smallestMeasurement );
 
-            $(".img-desc").css('width', newWidth + "px" );
-            $(".img-desc").css('max-width', newWidth + "px" );
-            $(".img-title").css('width', newWidth + "px" );
-            $(".img-title").css('max-width', newWidth + "px" );
-            $(".navigation-keys").css('width', newWidth + "px" );
+            $(".img-desc").css('width', smallestMeasurement + "px" );
+            $(".img-desc").css('max-width', smallestMeasurement + "px" );
+            $(".img-title").css('width', smallestMeasurement + "px" );
+            $(".img-title").css('max-width', smallestMeasurement + "px" );
+            $(".navigation-keys").css('width', smallestMeasurement + "px" );
+
+            var prevLeft = (browserWidth / 2) - (smallestMeasurement / 2) - 10; //x - $(".left-arrow").style.width;
+            var prevTop = browserHeight / 2; //y + (dy/2) - ($(".left-arrow").style.height / 2);
+            var nextLeft = (browserWidth / 2) + (smallestMeasurement / 2) + 30; //x + dx;
+            var nextTop = browserHeight / 2; //y + (dy/2) - ($(".right-arrow").style.height / 2);
+
+            $(".left-arrow").css('left', prevLeft + "px" );
+            $(".left-arrow").css('top', prevTop + "px" );
+            $(".right-arrow").css('left', nextLeft + "px" );
+            $(".right-arrow").css('top', nextTop + "px" );
         }
     };
 
